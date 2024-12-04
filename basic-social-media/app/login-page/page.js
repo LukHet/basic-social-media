c8bc1fc165e13cb13b9a0e2e0b549461863ba3b0
LukHet <lukaszhetman1@gmail.com>
2024-12-04 18:33:35 +0100

    Register validation

--- basic-social-media/app/login-page/page.js
+++ basic-social-media/app/login-page/page.js
@@ -8,15 +8,41 @@ import { useState } from "react";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const onButtonClick = async (e) => {
+    setErrorMessages([]);
+
+    if (!email || !emailRegex.test(email)) {
+      setErrorMessages((prevErrorMessages) => [
+        ...prevErrorMessages,
+        "Provide valid email address!",
+      ]);
+    }
+
+    if (!password || password.length < 8 || password.length > 64) {
+      setErrorMessages((prevErrorMessages) => [
+        ...prevErrorMessages,
+        "Provide a valid password longer than 8 characters!",
+      ]);
+    }
+
+    if (errorMessages.length > 0) {
+      e.preventDefault();
+      return;
+    }
+
     e.preventDefault();
     await axios
       .post("http://localhost:8080/user-register", {
         email: email,
         password: password,
       })
-      .then((res) => console.log(res));
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   };
 
   const handleEmailChange = (e) => {
@@ -42,12 +68,22 @@ export default function LoginPage() {
           <TextInput
             id="userpassword"
             type="password"
-            placeholder="password"
+            placeholder="Password"
             label="Password"
             onChange={handlePasswordChange}
             value={password}
           />
           <Button label="Register" onClick={onButtonClick} />
+
+          {errorMessages.length > 0 && (
+            <div>
+              {errorMessages.map((message, id) => (
+                <p key={id} className="text-red-500">
+                  {message}
+                </p>
+              ))}
+            </div>
+          )}
         </form>
       </main>
     </div>
